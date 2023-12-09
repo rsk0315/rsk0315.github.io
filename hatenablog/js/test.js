4c9667dad608091c5e064464523277b50af787a3
rsk0315 <rsk0315@icloud.com>
2023-12-09 20:55:39 +0900

    test

---
+++ hatenablog/js/test.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', (() => {
+  [...document.getElementsByTagName('pre')]
+    .filter((pre) => pre.getAttribute('data-lang') === 'test')
+    .forEach((pre) => {
+      pre.innerText = 'hello';
+    });
+  });
+}));
